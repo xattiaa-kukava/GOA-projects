081da2c93037469ff168d1550e69d207cafe7dbb
xutkaa <khutka009@gmail.com>
2024-09-10 19:34:09 +0400

    Add files via upload

---
+++ GOA-projectsss/DAY1/homework/homework1.py
@@ -0,0 +1,18 @@
+name = "xatia"
+surname = "kukava"
+age = 15
+height = 171
+i_actually_dont_know_my_height = True
+i_dont_have_birthday_in_a_week = False
+i_like_rock_music = True
+
+print(name)
+print(surname)
+print(age)
+print(height)
+print(i_actually_dont_know_my_height)
+print(i_dont_have_birthday_in_a_week)
+print(i_like_rock_music)
+
+
+print(name + " " + surname + " " + "is" + " " + str(age) + " " + str(height) + " " + "cm" + " " + "i actually dont know my height" + " " + "i have birthday in a week" + " " + "and" + " " + "i like rock music")
